506c0497001a43a452b7f74802b797a1cd612828
Jian-An Wang <facebook0983481181@gmail.com>
2022-04-19 11:37:58 +0800

    Finish first model training code

--- 2-segmentation_model_training.py
+++ 2-segmentation_model_training.py
@@ -1,3 +1,4 @@
+import gc
 import cv2
 import os
 import zipfile
@@ -7,16 +8,23 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torchvision import transforms
+from torchvision.models.segmentation import fcn_resnet50
 from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection impo rt train_test_split
+
 
 """
 
 程式撰寫大方向：
     1. 配對地匯入原始圖片與Mask
     2. 將所有圖片讀取
+    3. 將所有圖片轉換成 Tensor
     3. 切割訓練、測試資料
     4. 建立 TensorDataset, DataLoader
+    5. 定義模型、損失函數與優化器
 
 """
 
@@ -30,7 +38,63 @@ original_images = [plt.imread(zipData.open(i)) for i in tqdm(original_images, de
 mask_images = [plt.imread(zipData.open(i)) for i in tqdm(mask_images, desc = "Load Mask images")]
 mask_images = [np.where(i > 0, 1, 0) for i in mask_images]
 
+# 大方向 3：將所有圖片轉換成 Tensor
+image_transform = transforms.Compose([
+    transforms.ToTensor()
+])
+original_images_tensor = [image_transform(i).unsqueeze(0) for i in original_images]
+mask_images_tensor = [image_transform(i).unsqueeze(0) for i in mask_images]
+del original_images, mask_images
+gc.collect()
+
 # 大方向 3：切割訓練、測試資料
+xtrain, xtest, ytrain, ytest = train_test_split(original_images_tensor, mask_images_tensor, test_size = 0.2, shuffle = True)
+del original_images_tensor, mask_images_tensor # 待自動化後可使用
+gc.collect() # 待自動化後可使用
 
+train_tensordataset = TensorDataset(torch.concat(xtrain, axis = 0),
+                                    torch.concat(ytrain, axis = 0))
+test_tensordataset = TensorDataset(torch.concat(xtest, axis = 0),
+                                   torch.concat(ytest, axis = 0))
+del xtrain, ytrain, xtest, ytest # 待自動化後可使用
+gc.collect() # 待自動化後可使用
 
+# 大方向 4：建立 TensorDataset, DataLoader
+train_dataloader = DataLoader(train_tensordataset, batch_size = 1)
+test_dataloader = DataLoader(test_tensordataset, batch_size = 1)
+del train_tensordataset, test_tensordataset
+gc.collect()
 
+# 大方向 5：定義模型、損失函數與優化器
+class segmentation_transfer_learning(nn.Module):
+    def __init__(self):
+        super(segmentation_transfer_learning, self).__init__()
+        self.model = fcn_resnet50(pretrained = True)
+        self.decoder = nn.Sequential(
+            nn.Conv2d(in_channels = 21, out_channels = 1, kernel_size = 1),
+            nn.Sigmoid()
+        )
+        return
+
+    def forward(self, X):
+        X = self.model(X)
+        X = self.decoder(X["out"])
+        return X
+
+epochs = 10
+model = segmentation_transfer_learning()
+loss_func = nn.BCELoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr = 1e-4)
+train_loss_list, test_loss_list = list(), list()
+
+model.train()
+train_loss = 0.0
+for original_image, mask_image in train_dataloader:
+    optimizer.zero_grad()
+    yhat = model(original_image)
+
+    loss = loss_func(torch.flatten(yhat), torch.flatten(mask_image).float())
+    train_loss += loss.item()
+    loss.backward()
+    optimizer.step()
+    break
