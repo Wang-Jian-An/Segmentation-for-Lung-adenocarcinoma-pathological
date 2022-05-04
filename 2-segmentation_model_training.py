06a7d18fd45819eb18dc458e0738570ce607ae19
Jian-An, Wang <facebook0983481181@gmail.com>
2022-05-04 22:27:03 +0800

    Modify the model training code which load the batch number of image

--- 2-segmentation_model_training.py
+++ 2-segmentation_model_training.py
@@ -26,46 +26,38 @@ from sklearn.model_selection import train_test_split
     4. 建立 TensorDataset, DataLoader
     5. 定義模型、損失函數與優化器
 
+程式撰寫大方向（每次訓練前再輸入圖片的版本）：
+    1. 列出所有原始圖片、切片圖片的 ID
+    2. 將 ID 做訓練、測試資料的切割
+    3. 將訓練資料、測試資料進行 Batch
+    4. 定義模型、損失函數與優化器
+    5. 每個 Batch 的資料輪流取出來，進行後續的圖片讀取、圖片轉換成 Tensor、模型訓練或測試
+
 """
 
-# 大方向 1：配對地匯入原始圖片與Mask
-zipData = zipfile.ZipFile("..//SEG_Train_Mask_Images_Datasets.zip")
+# 設定預設路徑
+os.chdir("E://OneDrive - tmu.edu.tw//AI_Competition//肺腺癌病理切片影像之腫瘤氣道擴散偵測競賽 II：運用影像分割作法於切割STAS輪廓")
+
+# 大方向 1：列出所有原始圖片、切割圖片的 ID
+zipData = zipfile.ZipFile(".//SEG_Train_Mask_Images_Datasets.zip")
 original_images = sorted([i for i in zipData.namelist() if ("mask" not in i) and (".jpg" in i)])
 mask_images = sorted([i for i in zipData.namelist() if ("mask" in i) and (".jpg" in i)])
 
-# 大方向 2：將所有圖片讀取
-original_images = [plt.imread(zipData.open(i)) for i in tqdm(original_images, desc = "Load original Images")]
-mask_images = [plt.imread(zipData.open(i)) for i in tqdm(mask_images, desc = "Load Mask images")]
-mask_images = [np.where(i > 0, 1, 0) for i in mask_images]
+# 大方向 2：將 ID 做訓練、測試資料的切割
+xtrain_id, xtest_id, ytrain_id, ytest_id = train_test_split(original_images, mask_images, shuffle = True, test_size = 0.2)
 
-# 大方向 3：將所有圖片轉換成 Tensor
-image_transform = transforms.Compose([
-    transforms.ToTensor()
-])
-original_images_tensor = [image_transform(i).unsqueeze(0) for i in original_images]
-mask_images_tensor = [image_transform(i).unsqueeze(0) for i in mask_images]
-del original_images, mask_images
-gc.collect()
+# 大方向 3：將訓練資料、測試資料進行 Batch
+batch_size = 2
+train_set_group = (xtrain_id.__len__() // batch_size) + 1
+test_set_group = (xtest_id.__len__() // batch_size) + 1
+trainset_batch = [(xtrain_id[one_batch * batch_size: one_batch * batch_size + batch_size], ytrain_id[one_batch * batch_size: one_batch * batch_size + batch_size])
+                  if one_batch + 1 != train_set_group else (xtrain_id[one_batch * batch_size: ], ytrain_id[one_batch * batch_size: ]) for one_batch in range(train_set_group)]
+testset_batch = [(xtrain_id[one_batch * batch_size: one_batch * batch_size + batch_size], ytrain_id[one_batch * batch_size: one_batch * batch_size + batch_size])
+                  if one_batch + 1 != train_set_group else (xtrain_id[one_batch * batch_size: ], ytrain_id[one_batch * batch_size: ]) for one_batch in range(test_set_group)]
+# print(testset_batch)
 
-# 大方向 3：切割訓練、測試資料
-xtrain, xtest, ytrain, ytest = train_test_split(original_images_tensor, mask_images_tensor, test_size = 0.2, shuffle = True)
-del original_images_tensor, mask_images_tensor # 待自動化後可使用
-gc.collect() # 待自動化後可使用
 
-train_tensordataset = TensorDataset(torch.concat(xtrain, axis = 0),
-                                    torch.concat(ytrain, axis = 0))
-test_tensordataset = TensorDataset(torch.concat(xtest, axis = 0),
-                                   torch.concat(ytest, axis = 0))
-del xtrain, ytrain, xtest, ytest # 待自動化後可使用
-gc.collect() # 待自動化後可使用
-
-# 大方向 4：建立 TensorDataset, DataLoader
-train_dataloader = DataLoader(train_tensordataset, batch_size = 1)
-test_dataloader = DataLoader(test_tensordataset, batch_size = 1)
-del train_tensordataset, test_tensordataset
-gc.collect()
-
-# 大方向 5：定義模型、損失函數與優化器
+# 大方向 4：定義模型、損失函數與優化器
 class segmentation_transfer_learning(nn.Module):
     def __init__(self):
         super(segmentation_transfer_learning, self).__init__()
@@ -81,18 +73,33 @@ class segmentation_transfer_learning(nn.Module):
         X = self.decoder(X["out"])
         return X
 
-epochs = 10
 model = segmentation_transfer_learning()
 loss_func = nn.BCELoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr = 1e-4)
-scheduler = torch.optim.lr_scheduler(optimizer)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.9)
+
+# 大方向 5：每個 Batch 的資料輪流取出來，進行後續的圖片讀取、圖片轉換成 Tensor、模型訓練或測試
+epochs = 10
 train_loss_list, test_loss_list = list(), list()
 train_loss, vali_loss = 0.0, 0.0
+
+image_transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
 
 for epoch in range(epochs):
     train_loss, vali_loss = 0.0, 0.0
     model.train()
-    for original_image, mask_image in train_dataloader:
+    for original_image, mask_image in trainset_batch:
+        # 讀取圖片
+        original_image = [plt.imread(zipData.open(i)) for i in tqdm(original_image, desc = "Load original Images")]
+        mask_image = [plt.imread(zipData.open(i)) for i in tqdm(mask_image, desc = "Load Mask images")]
+
+        # 將圖片轉換成 Tensor
+        original_image = torch.concat([image_transform(i).unsqueeze(0) for i in original_image], axis = 0)
+        mask_image = torch.concat([image_transform(i).unsqueeze(0) for i in mask_image], axis = 0)
+
         optimizer.zero_grad()
         yhat = model(original_image)
 
@@ -104,13 +111,24 @@ for epoch in range(epochs):
     scheduler.step()
 
     model.eval()
-    for original_image, mask_image in test_dataloader:
+    for original_image, mask_image in testset_batch:
+        # 讀取圖片
+        original_image = [plt.imread(zipData.open(i)) for i in tqdm(original_image, desc = "Load original Images")]
+        mask_image = [plt.imread(zipData.open(i)) for i in tqdm(mask_image, desc = "Load Mask images")]
+
+        # 將圖片轉換成 Tensor
+        original_image = torch.concat([image_transform(i).unsqueeze(0) for i in original_image], axis = 0)
+        mask_image = torch.concat([image_transform(i).unsqueeze(0) for i in mask_image], axis = 0)
+
         with torch.no_grad():
             yhat = model(original_image)
+
         loss = loss_func(torch.flatten(yhat), torch.flatten(mask_image).float())
         vali_loss += loss.item()
+        break
     train_loss_list.append(train_loss)
     test_loss_list.append(vali_loss)
-
-    torch.save(model, "model//segmentation_fcn_resnet50.pth")
-
+    break
+    # torch.save(model, "model//segmentation_fcn_resnet50.pth")
+print(train_loss_list)
+print(test_loss_list)
